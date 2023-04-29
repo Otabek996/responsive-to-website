1904af217dd3c402d77e6a4e799bfe7affd8dd6c
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-04-30 00:05:59 +0500

    Added sildebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import styles from '../../styles/Sidebar.module.css';
+
+import { ROUTES } from "../../utils/routes";
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <section className={styles.sidebar}>
+      <div className={styles.sidebar__container}>
+        <div className={styles.sidebar__logo}>
+          <Link className={styles.logo_img} to={ROUTES.HOME}>
+            <span className={styles.logo_letter_first}>T</span>
+            <span className={styles.logo_letter_second}>O</span>
+          </Link>
+        </div>
+
+        <div className={styles.social__networks}>
+          <div className={styles.network__box}>
+            <a rel="noreferrer" href="https://github.com/Otabek996" target='_blank'>
+              <i class="ri-github-fill"></i>
+            </a>
+          </div>
+
+          <div className={styles.network__box}>
+            <a rel="noreferrer" href="https://www.instagram.com/0tabek.t/" target='_blank'>
+              <i class="ri-instagram-line"></i>
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
   )
 }
 
