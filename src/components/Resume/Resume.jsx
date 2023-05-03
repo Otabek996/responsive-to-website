fd48518e23df019b391f49e2bc29daab2d89e88f
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-05-03 15:40:33 +0500

    Added cv link

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -110,6 +110,17 @@ const Resume = () => {
             </div>
           </div>
         </div>
+
+        <div className={styles.border}></div>
+
+        <div className={styles.resume__section}>
+          <button className={styles.btn__cv} type='button'>
+            <a href="https://tashkent.hh.uz/applicant/resumes/view?resume=307c12b9ff0ba58d670039ed1f725553324846" target='_blank' rel="noreferrer">
+              <span>CV</span>
+              <span className={styles.btn__cv_icon}><i className="ri-arrow-right-up-line"></i></span>
+            </a>
+          </button>
+        </div>
       </div>
     </section>
   )
