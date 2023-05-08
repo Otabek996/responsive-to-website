3f430604f9f5edb06043d75ecc35753b59104d1c
Tuychiev Otabek <otabekotabek996996@gmail.com>
2023-05-08 14:06:18 +0500

    �️�️�️�️�️�️�️Fixed bug

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -2,18 +2,7 @@ import React from 'react';
 
 import styles from '../../styles/Resume.module.css';
 
-import HTML from '../../images/html.png';
-import CSS from '../../images/css.svg';
-import SASS from '../../images/sass.png';
-import BOOTSTRAP from '../../images/bootstrap.png';
-import JS from '../../images/js.png';
-import REACT from '../../images/react.png';
-import TYPESCRIPT from '../../images/typescript.svg';
-import NEXTJS from '../../images/next-js.svg';
-import GITHUB from '../../images/github.svg';
-import GIT from '../../images/git.png';
-import NPM from '../../images/npm.avif';
-import NETLIFY from '../../images/netlify.svg';
+import { techs, othersTechs } from '../../utils/techData';
 
 const Resume = () => {
   return (
@@ -28,53 +17,13 @@ const Resume = () => {
             <h2 className={styles.tech__box_title}>The tech I use for building client-side applications</h2>
 
             <div className={styles.techs}>
-              <div className={styles.tech}>
-                <span title='html'>
-                  <img className={styles.tech__img} src={HTML} alt="html" />
-                </span>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='css'>
-                  <img className={styles.tech__img} src={CSS} alt="css" />
-                </span>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='sass'>
-                  <img className={styles.tech__img} src={SASS} alt="sass" />
-                </span>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='bootstrap'>
-                  <img className={styles.tech__img} src={BOOTSTRAP} alt="bootstrap" />
-                </span>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='javascript'>
-                  <img className={styles.tech__img} src={JS} alt="javascript" />
-                </span>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='react'>
-                  <img className={styles.tech__img} src={REACT} alt="react" />
-                </span>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='nextjs'>
-                  <img className={styles.tech__img} src={NEXTJS} alt="nextjs" />
-                </span>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='typescript'>
-                  <img className={styles.tech__img} src={TYPESCRIPT} alt="typescript" />
-                </span>
-              </div>
+              {techs.map((tech) => (
+                <div className={styles.tech} key={tech.id}>
+                  <span title={tech.text}>
+                    <img className={styles.tech__img} src={tech.img} alt={tech.text} />
+                  </span>
+                </div>
+              ))}
             </div>
           </div>
 
@@ -82,31 +31,13 @@ const Resume = () => {
             <h2 className={styles.tech__box_title}>Others techs</h2>
 
             <div className={styles.techs}>
-              <div className={styles.tech}>
-                <a href="https://github.com/Otabek996" target='_blank' rel="noreferrer">
-                  <span title='github'>
-                    <img className={styles.tech__img} src={GITHUB} alt="github" />
+              {othersTechs.map((otherTech) => (
+                <div className={styles.tech} key={otherTech.id}>
+                  <span title={otherTech.text}>
+                    <img className={styles.tech__img} src={otherTech.img} alt={otherTech.text} />
                   </span>
-                </a>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='git'>
-                  <img className={styles.tech__img} src={GIT} alt="git" />
-                </span>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='netlify'>
-                  <img className={styles.tech__img} src={NETLIFY} alt="netlify" />
-                </span>
-              </div>
-
-              <div className={styles.tech}>
-                <span title='npm'>
-                  <img className={styles.tech__img} src={NPM} alt="npm" />
-                </span>
-              </div>
+                </div>
+              ))}
             </div>
           </div>
         </div>
---
+++ src/utils/techData.js
@@ -0,0 +1,81 @@
+// Techs img
+import HTML from '../images/html.png';
+import CSS from '../images/css.svg';
+import SASS from '../images/sass.png';
+import BOOTSTRAP from '../images/bootstrap.png';
+import JS from '../images/js.png';
+import REACT from '../images/react.png';
+import TYPESCRIPT from '../images/typescript.svg';
+import NEXTJS from '../images/next-js.svg';
+import GITHUB from '../images/github.svg';
+import GIT from '../images/git.png';
+import NPM from '../images/npm.avif';
+import NETLIFY from '../images/netlify.svg';
+
+// The tech I use
+export const techs = [
+    {
+        id: 1,
+        text: 'html',
+        img: HTML
+    },
+    {
+        id: 2,
+        text: 'css',
+        img: CSS
+    },
+    {
+        id: 3,
+        text: 'sass',
+        img: SASS
+    },
+    {
+        id: 4,
+        text: 'bootstrap',
+        img: BOOTSTRAP
+    },
+    {
+        id: 5,
+        text: 'javascript',
+        img: JS
+    },
+    {
+        id: 6,
+        text: 'react',
+        img: REACT
+    },
+    {
+        id: 7,
+        text: 'nextjs',
+        img: NEXTJS
+    },
+    {
+        id: 8,
+        text: 'typescript',
+        img: TYPESCRIPT
+    },
+]
+
+// Others techs
+export const othersTechs = [
+    {
+        id: 1,
+        text: 'github',
+        img: GITHUB
+    },
+    {
+        id: 2,
+        text: 'git',
+        img: GIT
+    },
+    {
+        id: 3,
+        text: 'netlify',
+        img: NETLIFY
+    },
+    {
+        id: 4,
+        text: 'npm',
+        img: NPM
+    },
+]
